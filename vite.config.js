25dadfbcdc79c011c36a4ef950722363f396946f
Mariana Contreras <115504201+Marualexa@users.noreply.github.com>
2023-09-26 18:36:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import {resolve} from 'path';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
-  base: './web-genesis/',
+  base: '/webGenesis/',
+  resolve: {
+    alias: {
+      '@img': fileURLToPath(new URL('src/assets/', import.meta.url)),
+      '@': fileURLToPath(new URL('src/', import.meta.url)),
+      
+
+    }
+  },
 })
