18b68f4a0668486e4bf8da3a84cb3037542dfe92
Mariana <contrerasomanam@gmail.com>
2023-10-03 18:20:23 -0500

    Adaptacion de diseño responsivos #6

--- src/components/CarrucelTesting.vue
+++ src/components/CarrucelTesting.vue
@@ -17,7 +17,7 @@
 
                   <div class="img-inf">
                     <img class="est" src="@img/estrellas.png" alt="" />
-                    <img src="@img/icon.svg" alt="" />
+                    <img :src="item.imageSrc" alt="" />
                   </div>
                 </div>
               </v-card>
@@ -31,11 +31,7 @@
             alt="Imagen Anterior"
             @click="model = Math.max(model - 1, 0)"
           />
-          <img
-            src="@img/boton.svg"
-            alt="Contador"
-            style="width: 30px; height: 30px"
-          />
+          <img src="@img/boton.svg" alt="Contador" style="width: 30px; height: 30px" />
           <img
             width="20"
             src="@img/flecha-right.svg"
@@ -49,6 +45,12 @@
 </template>
 
 <script>
+import jonatan from "../assets/Jhonatan.jpeg";
+import angie from "../assets/Angie.jpeg";
+import paula from "../assets/Paula.jpeg";
+import ivonne from "../assets/Ivonne.jpeg";
+import drenk from "../assets/Drenk.jpeg";
+import icon from "../assets/icon.svg";
 export default {
   data() {
     return {
@@ -56,39 +58,47 @@ export default {
         {
           name: "Jonatan Gomez",
           message: "Gracias por tus servicios, siempre que puedo te recomiendo.",
+          imageSrc: jonatan,
         },
         {
           name: "Angie Bautista",
           message:
             "La mejor siempre, todo lo relacionado con vuelo, ella siempre es la mejor.",
+          imageSrc: angie,
         },
         {
           name: "Paula Zuluaga",
           message:
             "Cuando quiero viajar siempre confió en ella, es la mejor y me consigue los mejores precios.",
+          imageSrc: paula,
         },
         {
           name: "Ivonne Gomez",
           message: "Gracias por estar pendiente durante el viaje, eres muy responsable.",
+          imageSrc: ivonne,
         },
         {
           name: "Drenk Music",
           message:
             "Ella me hace todo, sea la hora que sea, me busca buenas ofertas y me hace cada Check In.",
+          imageSrc: drenk,
         },
         {
           name: "Gladys Aranda",
           message:
             "Sé que estuve intensa, porque por falta de documentos me devolvieron de un viaje, pero de verdad gracias por estar tan preparada con todo.",
+          imageSrc: icon,
         },
         {
           name: "Andrea Rodriguez",
           message: "Seguiré viajando contigo, has sido de gran ayuda.",
+          imageSrc: icon,
         },
         {
           name: "Gonzalo Nieto",
           message:
             "Nos ayudaste demasiado, es imposible no seguir viajando contigo y recomendarte es un placer.",
+          imageSrc: icon,
         },
       ],
       model: 0,
