b7ce599c8bef95c40bc027de741a91feba37a3f6
GenesisSuarez <agenttravel641@gmail.com>
2023-11-23 18:27:41 -0500

    arreglar rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
-    { path: '/Web-genesis/', name: 'ImportApp', component: () => import('../components/ImportApp.vue') },
-    { path: '/Web-genesis/addFormy', name: 'form', component: () => import('../components/formularioDeRegistro/AddForm.vue') },
-    { path: '/Web-genesis/logIn', name: 'logIn', component: () => import('../components/InfoClientes/LogIn.vue') },
-    { path: '/Web-genesis/infoTable', name: 'infoTable', component: () => import('../components/InfoClientes/InforTable.vue') },
+    // { path: '/Web-genesis', name: 'ImportApp', component: () => import('../components/ImportApp.vue') },
+    // { path: '/Web-genesis/addFormy', name: 'form', component: () => import('../components/formularioDeRegistro/AddForm.vue') },
+    // { path: '/Web-genesis/logIn', name: 'logIn', component: () => import('../components/InfoClientes/LogIn.vue') },
+    // { path: '/Web-genesis/infoTable', name: 'infoTable', component: () => import('../components/InfoClientes/InforTable.vue') },
+    { path: '/', name: 'ImportApp', component: () => import('../components/ImportApp.vue') },
+    { path: '/addFormy', name: 'form', component: () => import('../components/formularioDeRegistro/AddForm.vue') },
+    { path: '/logIn', name: 'logIn', component: () => import('../components/InfoClientes/LogIn.vue') },
+    { path: '/infoTable', name: 'infoTable', component: () => import('../components/InfoClientes/InforTable.vue') },
   ]
 });
 
