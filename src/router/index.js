ec7d589df3a9760ead57bc768c95eac5c3b78d8a
GenesisSuarez <agenttravel641@gmail.com>
2023-11-23 18:15:43 -0500

    Arreglo#7

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,9 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/Web-genesis/', name: 'ImportApp', component: () => import('../components/ImportApp.vue') },
-    { path: '/Web-genesis/addFormy/', name: 'form', component: () => import('../components/formularioDeRegistro/AddForm.vue') },
-    { path: '/Web-genesis/logIn/', name: 'logIn', component: () => import('../components/InfoClientes/LogIn.vue') },
-    { path: '/Web-genesis/infoTable/', name: 'infoTable', component: () => import('../components/InfoClientes/InforTable.vue') },
+    { path: '/Web-genesis/addFormy', name: 'form', component: () => import('../components/formularioDeRegistro/AddForm.vue') },
+    { path: '/Web-genesis/logIn', name: 'logIn', component: () => import('../components/InfoClientes/LogIn.vue') },
+    { path: '/Web-genesis/infoTable', name: 'infoTable', component: () => import('../components/InfoClientes/InforTable.vue') },
   ]
 });
 
