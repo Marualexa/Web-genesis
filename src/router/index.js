8ecac32a6dbc2d679b50cfeb41a461042ec29d75
GenesisSuarez <agenttravel641@gmail.com>
2023-11-23 18:03:32 -0500

    Arreglo#3

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     { path: '/Web-genesis/', name: 'ImportApp', component: () => import('../components/ImportApp.vue') },
     { path: '/Web-genesis/addFormy/', name: 'form', component: () => import('../components/formularioDeRegistro/AddForm.vue') },
